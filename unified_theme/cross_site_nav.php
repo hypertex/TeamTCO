31ef4aae423427069927addf5787e6f6d6c57c21
Joel Helbling <joel@adelphus.com>
2009-07-18 02:09:36 -0400

    created cross_site_nav menu (shared)

---
+++ unified_theme/cross_site_nav.php
@@ -0,0 +1,6 @@
+<ul class="sitenav">
+	<li><a href="http://techcorpsohio.org">tech corps ohio</a></li>
+	<li><a href="http://studenttechcorps.org/moodle">student tech corps</a></li>
+	<li><a href="http://clubtechcorps.org">club tech corps</a></li>
+	<li><a href="http://techcorpsvolunteer.org">tech corps volunteer</a></li>
+</ul>
