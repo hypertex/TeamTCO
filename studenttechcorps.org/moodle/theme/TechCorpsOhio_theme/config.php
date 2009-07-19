314d5b848db8ec8596a855ebe66b7b19ceb36dd4
Sarah Dutkiewicz <sarah@codinggeekette.com>
2009-07-19 13:03:54 -0400

    Updating the theme name

--- studenttechcorps.org/moodle/theme/TechCorpsOhio_theme/config.php
+++ studenttechcorps.org/moodle/theme/TechCorpsOhio_theme/config.php
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-$THEME->sheets = array('cornflower');
+$THEME->sheets = array('TechCorpsOhio_theme');
 
 /// This variable is an array containing the names of all the 
 /// stylesheet files you want included in this theme, and in what order
